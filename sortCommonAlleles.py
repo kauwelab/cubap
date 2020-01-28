f04866c75e3b5872100d2810ca0eb241f730a50d
Matt Hodgman <matthodg1@gmail.com>
2020-01-28 14:44:59 -0700

    added functionality of merging nucleotide composition CSVs

--- sortCommonAlleles.py
+++ sortCommonAlleles.py
@@ -2,12 +2,14 @@ import csv
 import argparse
 
 def formatRow(row):
-    string_row = row[0] + ',' + row[1]
     if args.cotrna:
-        for x in range(2,24):
-            string_row += ',' + str(int(float(row[x])))
+        headerLength = 24
+    elif args.nucleotides:
+        headerLength = 6
     else:
-        for x in range(2,66):
+        headerLength = 66
+    string_row = row[0] + ',' + row[1]
+        for x in range(2,headerLength):
             string_row += ',' + str(int(float(row[x])))
     return string_row
 
@@ -17,6 +19,7 @@ def parseArgs():
     parser.add_argument("-o", "--output",help="Output File",action="store",dest="output", required=True)
     parser.add_argument("-c", "--cotrna",help="flag to merge co-tRNA codon pairing CSV files", action="store_true",dest="cotrna", required=False)
     parser.add_argument("-s", "--samples",help="include sample names in output CSV", action="store_true",dest="samples",required=False)
+    parser.add_argument("-n", "--nucleotides",help="flag to merge nucleotide composition CSV files", action="store_true",dest="nucleotides",required=False)
     args = parser.parse_args()
     return args
 
@@ -30,7 +33,7 @@ for x in allInputFiles:
         reader = csv.reader(f)
         for row in reader:
             if row[0] == 'Sample':
-                codons = row[3:]
+                columns = row[3:]
             else:
                 gene = formatRow(row[1:])
                 if gene in myDict.keys():
@@ -42,7 +45,7 @@ for x in allInputFiles:
 with open(outFile, 'w') as csv_file:
     writer = csv.writer(csv_file)
     if args.samples:
-        writer.writerow(['Index', 'Gene', 'Name', 'Samples'] + codons)
+        writer.writerow(['Index', 'Gene', 'Name', 'Samples'] + columns)
         i = 1
         for key,value in myDict.items():
             gene_row = key.split(',')
@@ -50,7 +53,7 @@ with open(outFile, 'w') as csv_file:
             writer.writerow([i] + gene_row[0:1] + [gene_row[1]] + [sampleList] + gene_row[2:])
             i += 1
     else:
-        writer.writerow(['Index', 'Gene', 'Name'] + codons)
+        writer.writerow(['Index', 'Gene', 'Name'] + columns)
         i = 1
         for key,value in myDict.items():
             gene_row = key.split(',')
