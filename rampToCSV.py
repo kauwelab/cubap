af47b25115d4440412f721e704fba97b4ac9a2ae
Matt Hodgman <matthodg1@gmail.com>
2022-05-10 21:27:44 -0600

    fixed indentation errors

--- rampToCSV.py
+++ rampToCSV.py
@@ -3,18 +3,15 @@ import argparse
 import pandas as pd
 import statistics
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-e", "--efficiencies", help="Input FASTA Files of codon translation efficiencies as RSCU values",nargs='*',action="store", dest="input", required=True)
-parser.add_argument("-r", "--ramps", help="Input FASTA Files containing the ramp sequences",nargs='*',action="store", dest="ramps", required=True)
-parser.add_argument('-k', '--keyFile', default='sampleKeyPop.csv', help="a csv key for identifying the sample's population")
-parser.add_argument('-c', '--chart', action='store_true', help="output CSV only contains Gene Harmonic Mean RSCU, Ramp Harmonic Mean RSCU, Gene Length, Ramp Length", required=False)
-parser.add_argument('-o', '--output', help="directory to which output files are saved")
-args = parser.parse_args()
-allInputFiles = args.efficiencies
-allRampFiles = args.ramps
-chart = args.chart
-outputDir = args.output
-df_pop = pd.read_csv(args.keyFile)
+def parseArgs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-e", "--efficiencies", help="Input FASTA Files of codon translation efficiencies as RSCU values",nargs='*',action="store", dest="efficiencies", required=True)
+    parser.add_argument("-r", "--ramps", help="Input FASTA Files containing the ramp sequences",nargs='*',action="store", dest="ramps", required=True)
+    parser.add_argument('-k', '--keyFile', default='sampleKeyPop.csv', help="a csv key for identifying the sample's population")
+    parser.add_argument('-c', '--chart', action='store_true', help="output CSV only contains Gene Harmonic Mean RSCU, Ramp Harmonic Mean RSCU, Gene Length, Ramp Length", required=False)
+    parser.add_argument('-o', '--output', help="directory to which output files are saved")
+    args = parser.parse_args()
+    return args
 
 def make_column_number_list(d):
     longest = 0
@@ -71,6 +68,7 @@ def to_csv_chart(sample, subpopulation, superpopulation, d, d_A):
                 i += 1
                 geneRSCU, rampRSCU = get_harmonic_means(ramp, rampLen)
                 writer.writerow([sample] + [subpopulation] + [superpopulation] + [gene.split(' ')[0]] + [gene.split(' ')[1]] + [str(geneRSCU)] + [str(rampRSCU)] + [str(len(ramp))] + [str(rampLen)])
+
 def to_csv(sample, subpopulation, superpopulation, d, d_A):
     numList = make_column_number_list(d)
     if len(make_column_number_list(d_A)) > len(make_column_number_list(d)):
@@ -98,55 +96,70 @@ def get_gene_name(description):
     Extracts the gene name and NP number from the description
     '''
     geneName = description[description.find("gene=") + 5 :
-        description.find("gene=") + 5 +
-        get_next_semicolon(description[description.find("gene=") + 5 : ])]
+    description.find("gene=") + 5 +
+    get_next_semicolon(description[description.find("gene=") + 5 : ])]
+
     try:
         np = ' ' + description[description.find("Name=") + 5 :
-            description.find("Name=") + 5 +
-            get_next_semicolon(description[description.find("Name=") + 5 : ])]
+        description.find("Name=") + 5 +
+        get_next_semicolon(description[description.find("Name=") + 5 : ])]
     except:
         np = ''
+
     return geneName + np
 
 if __name__ =='__main__':
     '''
     Main.
     '''
-d = dict()
-d_A = dict()
-setty = set()
-is_description = False
-for x in allInputFiles:
-    with open(x, 'r') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            ramp = []
-            if is_description == False and str(row[0])[0] == '>':
-                is_description = True
-                gene = get_gene_name(str(row))
-            elif is_description == True and str(row[0])[0] != '>':
-                if chart:
-                    for rscu in row:
-                        ramp.append(float(rscu.strip()))
-                else:
-                    for rscu in row:
-                        if float(rscu.strip()) == 1.0:
-                            ramp.append(int(float(rscu.strip())))
-                        else:
-                            ramp.append(round(float(rscu.strip()),3))
-                is_description = False
-                if gene in d.keys():
-                    d_A[gene] = ramp
-                else:
-                    d[gene] = ramp
-        if x.find('.') == -1:
-	        sampleName = x[-7:]
-        else:
-	        sampleName = x[x.find('.')-7:x.find('.')]
-        subpopulation = df_pop[df_pop['Sample'] == sample.rstrip()]['Subpopulation'].item()
-        superpopulation = df_pop[df_pop['Sample'] == sample.rstrip()]['Superpopulation'].item()
-        if chart:
-            to_csv_chart(sample, subpopulation, superpopulation, d, d_A)
-        else:
-            to_csv(sample, subpopulation, superpopulation, d, d_A)
-        print(sample)
+    args = parseArgs()
+    allInputFiles = args.efficiencies
+    allRampFiles = args.ramps
+    chart = args.chart
+    outputDir = args.output
+    df_pop = pd.read_csv(args.keyFile)
+
+    d = dict()
+    d_A = dict()
+    setty = set()
+    is_description = False
+
+    for x in allInputFiles:
+        with open(x, 'r') as f:
+            reader = csv.reader(f)
+
+            for row in reader:
+                ramp = []
+                if is_description == False and str(row[0])[0] == '>':
+                    is_description = True
+                    gene = get_gene_name(str(row))
+                elif is_description == True and str(row[0])[0] != '>':
+                    if chart:
+                        for rscu in row:
+                            ramp.append(float(rscu.strip()))
+                    else:
+                        for rscu in row:
+                            if float(rscu.strip()) == 1.0:
+                                ramp.append(int(float(rscu.strip())))
+                            else:
+                                ramp.append(round(float(rscu.strip()),3))
+                    is_description = False
+                    if gene in d.keys():
+                        d_A[gene] = ramp
+                    else:
+                        d[gene] = ramp
+
+            if x.find('.') == -1:
+                sampleName = x[-7:]
+            else:
+                sampleName = x[x.find('.')-7:x.find('.')]
+                print(sampleName)
+                subpopulation = df_pop[df_pop['Sample'] == sampleName.rstrip()]['Subpopulation'].item()
+                superpopulation = df_pop[df_pop['Sample'] == sampleName.rstrip()]['Superpopulation'].item()
+                
+            if chart:
+                to_csv_chart(sampleName, subpopulation, superpopulation, d, d_A)
+            else:
+                to_csv(sampleName, subpopulation, superpopulation, d, d_A)
+            
+            print(sampleName)
